9e2d663fba3168ce291609acaf2f7c3ee122dede
Sam Cogan <mail@samcogan.com>
2019-11-10 18:03:23 +0000

    Fix syntax

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "azurerm_subnet" "aks_subnet" {
   resource_group_name  = "${azurerm_resource_group.rg.name}"
   virtual_network_name = "${azurerm_virtual_network.vnet.name}"
   address_prefix       = "10.0.1.0/24"
-  depends_on = null_resource.azureCLI
+  depends_on = [null_resource.azureCLI]
 
 
 
